44e20e15301d78a95d1c33ebe7110bc9f11d74d4
NISH1001 <nishanpantha@gmail.com>
2019-08-12 06:57:01 +0545

    Convert to python3 code and dump combined meaning as well

--- WordFreq.py
+++ WordFreq.py
@@ -12,8 +12,7 @@ WordLists ( Courtesy: www.quizlet.com )
 Above words are stored in different text files along with definations. These text files are combined, parsed and word frequecy is calculated.
 Word along with Frequenices is stored in output text file 'output.txt'
 """
-
-print "Welcome to Word Frequency Sorter!!\n"
+from collections import defaultdict
 
 # Function to calculate word Frequency and store it into Dictionary
 def wordListToFreqDict(wordlist):
@@ -24,25 +23,38 @@ def wordListToFreqDict(wordlist):
 filenames = ['WordLists/Kaplan.txt','WordLists/Magoosh.txt','WordLists/Manhattan.txt','WordLists/Barrons.txt','WordLists/Princeton.txt','WordLists/Majortest.txt']
 with open('CombinedAllWords.txt', 'w') as outfile:
     for fname in filenames:
-        with open(fname) as infile:
+        with open(fname, encoding="ISO-8859-1") as infile:
             for line in infile:
-                line =line.lower()
+                line = line.lower()
                 outfile.write(line)
 
 # Open combined wordlist, remove definations and format words
 with open("CombinedAllWords.txt") as f:
     items = f.readlines()
-    t = items
-    t = map(lambda items: items.strip(), t)
-    for i, w in enumerate(t):
-        t[i]=w.split(":", 1)[0]
+    combined_vocab = items
+    combined_vocab = list(map(lambda items: items.strip(), combined_vocab))
+    for i, w in enumerate(combined_vocab):
+        combined_vocab[i]=w.split(":", 1)[0]
 
-#Clear contents of output text file.
-l = open("Words_Output.txt","w").close()
+# my custom code to create a super dictionary
+combined_map = defaultdict(list)
+with open("CombinedAllWords.txt") as f:
+    for line in f:
+        line = line.strip()
+        splitted =  line.split(':')
+        if len(splitted) > 1:
+            word, meaning = splitted[0], splitted[1]
+            combined_map[word].append(meaning)
+
 
 #Open file and write sorted dictionary values into text file one by one in decreasing order
-l = open("Words_Output.txt","a")
-l.write("Most Frequent Words: \n")
-for key, value in sorted(wordListToFreqDict(t).iteritems(), key=lambda (k,v): (v,k),reverse =True):
-    l.write(" \n%s: %s" % (key, value))
+l = open("Words_Output.txt","w")
+
+# for key, value in sorted(wordListToFreqDict(t).items(), key=lambda k,v: (v,k), reverse =True):
+for key, value in sorted(wordListToFreqDict(combined_vocab).items(), key=lambda x: (x[1], x[0]), reverse =True):
+    # l.write(" \n%s: %s" % (key, value))
+    word = key
+    freq = value
+    meaning = combined_map[word]
+    l.write("{}[{}]: {}\n".format(key, value, meaning))
 l.close()
